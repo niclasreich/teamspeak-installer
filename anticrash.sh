f57401030e0f9be6b1521d51e4055f59d8f79548
Niclas <contact@naspyy.eu>
2017-04-20 22:04:46 +0200

    First Commit

---
+++ anticrash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TEAMSPEAK=`ps ax | grep ts3server_linux_amd64 | grep -v grep | wc -l`
+if [ $TEAMSPEAK -eq 1 ]; then
+	exit
+	else
+	cd /opt/teamspeak || return
+	./ts3server_startscript.sh start || exit
+fi
