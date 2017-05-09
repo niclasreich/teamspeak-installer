2f32e6b569119a9ed53e501f493a8e1af5b1f777
Niclas <contact@naspyy.eu>
2017-05-09 19:09:33 +0200

    Update anticrash.sh

--- anticrash.sh
+++ anticrash.sh
@@ -3,6 +3,6 @@ TEAMSPEAK=`ps ax | grep ts3server_linux_amd64 | grep -v grep | wc -l`
 if [ $TEAMSPEAK -eq 1 ]; then
 	exit
 	else
-	cd /opt/teamspeak || return
+	cd /opt/teamspeak/server || return
 	./ts3server_startscript.sh start || exit
 fi
