7b7c7eeba809b89f7afbe164cc490027ee525bd0
Niclas <contact@naspyy.eu>
2017-05-14 11:14:44 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #source: https://r4p3.net/threads/teamspeak-3-server-automatic-backup-script.1582/#post-13350
-zip -r "Backup from $(date +"%d.%m.%Y at %H:%M:%S").zip" /opt/teamspeak/
+zip -r "Backup from $(date +"%d.%m.%Y at %H:%M:%S").zip" /opt/teamspeak/server/
