99f6546e0134297f69fa09e5b435704357e0d9ea
Niclas Reich <kontakt@niclasreich.de>
2018-03-07 15:45:05 +0100

    Update update-3.1.0-teamspeak-user.sh

--- update-3.1.0-teamspeak-user.sh
+++ update-3.1.0-teamspeak-user.sh
@@ -4,7 +4,7 @@ cd server
 sleep 3
 cd ..
 cp -R server server_old
-wget http://dl.4players.de/ts/releases/3.1.0/teamspeak3-server_linux_amd64-3.1.0.tar.bz2
+wget http://dl.4players.de/ts/releases/3.1.1/teamspeak3-server_linux_amd64-3.1.1.tar.bz2
 mv server teamspeak3-server_linux_amd64
 tar -jxvf teamspeak3-server_linux*.tar.bz2
 mv teamspeak3-server_linux_amd64 server
