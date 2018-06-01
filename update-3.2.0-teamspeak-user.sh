0ea4a3cf7123d6a669a23f9fc46fb48925cb28eb
Niclas Reich <kontakt@niclasreich.de>
2018-06-01 11:50:22 +0200

    Update and rename update-3.1.0-teamspeak-user.sh to update-3.2.0-teamspeak-user.sh

--- update-3.2.0-teamspeak-user.sh
+++ update-3.2.0-teamspeak-user.sh
@@ -4,11 +4,11 @@ cd server
 sleep 3
 cd ..
 cp -R server server_old
-wget http://dl.4players.de/ts/releases/3.1.1/teamspeak3-server_linux_amd64-3.1.1.tar.bz2
+wget http://dl.4players.de/ts/releases/3.2.0/teamspeak3-server_linux_amd64-3.2.0.tar.bz2
 mv server teamspeak3-server_linux_amd64
 tar -jxvf teamspeak3-server_linux*.tar.bz2
 mv teamspeak3-server_linux_amd64 server
-rm teamspeak3-server_linux_amd64-3.1.0.tar.bz2
+rm teamspeak3-server_linux_amd64-3.2.0.tar.bz2
 cd server
 type nul > .ts3server_license_accepted
 ./ts3server_startscript.sh start
