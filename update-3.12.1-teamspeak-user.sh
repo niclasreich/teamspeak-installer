ea1d327854693a8ef5267d4ad9337cac397121b1
Razuuu <joshua@arrow-systems.de>
2020-11-16 15:55:11 +0100

    Update update-3.12.1-teamspeak-user.sh

--- update-3.12.1-teamspeak-user.sh
+++ update-3.12.1-teamspeak-user.sh
@@ -4,11 +4,11 @@ cd server
 sleep 3
 cd ..
 cp -R server server_old
-wget https://files.teamspeak-services.com/releases/server/3.12.1/teamspeak3-server_linux_amd64-3.12.1.tar.bz2
+wget https://files.teamspeak-services.com/releases/server/3.13.1/teamspeak3-server_linux_amd64-3.13.1.tar.bz2
 mv server teamspeak3-server_linux_amd64
 tar -jxvf teamspeak3-server_linux*.tar.bz2
 mv teamspeak3-server_linux_amd64 server
-rm teamspeak3-server_linux_amd64-3.12.1.tar.bz2
+rm teamspeak3-server_linux_amd64-3.13.1.tar.bz2
 cd server
 type nul > .ts3server_license_accepted
 ./ts3server_startscript.sh start
